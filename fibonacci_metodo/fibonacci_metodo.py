88d7ea97cbff2db32f4929c1828cff62ad6feaa3
Villagran <luisbmx_haro@hotmail.com>
2024-06-04 19:20:39 -0600

    metodo fibonacci

---
+++ fibonacci_metodo/fibonacci_metodo.py
@@ -0,0 +1,11 @@
+def fibonacci(num):
+    a,b = 0,1
+    fibonacci_lista=[0]
+    for i in range(num):
+        if b > num : return fibonacci_lista
+        else:
+            fibonacci_lista.append(b)
+            a,b = b,a+b
+
+resultado = fibonacci(33)
+print(resultado) 
