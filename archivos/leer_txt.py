6650074335e8be5f2284afc0eac0113a43660565
Villagran <luisbmx_haro@hotmail.com>
2024-06-07 22:08:20 -0600

    Formas de leer un archivo .txt y su segunda linea al igual que su forma mas optimizada

--- archivos/leer_txt.py
+++ archivos/leer_txt.py
@@ -1,1 +1,12 @@
-# Hola
+archivo = open("archivos\\texto_gerardo.txt", encoding="UTF-8")
+
+# texto_archivo = archivo.read()
+
+# lineas = archivo.readlines()
+
+linea_1 = archivo.readline()
+linea_2 = archivo.readline()
+
+archivo.close()
+
+print(linea_2)
---
+++ archivos/leer_txt_optimamente.py
@@ -0,0 +1,2 @@
+with open('archivos\\texto_gerardo.txt', encoding="UTF-8") as archivo:
+    print(archivo.read())
